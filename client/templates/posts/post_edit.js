a69e33989f737300d2d6728398b226b23f95abb5
stevenjmarsh <sjm.atl@gmail.com>
2015-07-09 15:55:36 -0700

    prevent editing of other post fields

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,17 @@
+/**
+ * For simple, straight forward edits such as the update and delete below, it
+ * is okay practice to do the update and remove operations here, with the allow
+ * set up.
+ *
+ * When the database operations become more than simple, it is better practice
+ * to set up a server method. (i.e., if adding preventing a duplicate URL on
+ * edit, like done on create, the below update code could/should be moved to
+ * a server method.
+ *
+ * For further explanation/examples see:
+ *      book.discovermeteor.com/chapter/editing-posts
+ *      (Method Calls vs Client-side Data Manipulation note section)
+ */
 Template.postEdit.events({
     'submit form': function (e) {
         e.preventDefault();
