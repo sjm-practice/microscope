494886edb150a0f71541c8c4ef2b3f7338107fc5
stevenjmarsh <sjm.atl@gmail.com>
2015-07-09 17:35:59 -0700

    error message

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -26,7 +26,7 @@ Template.postEdit.events({
 
         Posts.update(currentPostId, { $set: postProperties }, function (error) {
             if (error) {
-                alert(error.reason);
+                throwError(error.reason);
             } else {
                 Router.go('postPage', { _id: currentPostId });
             }
@@ -40,7 +40,7 @@ Template.postEdit.events({
             var currentPostId = this._id;
             Posts.remove(currentPostId, function (error) {
                 if (error) {
-                    alert(error.reason);
+                    throwError(error.reason);
                 } else {
                     Router.go('postsList');
                 }
