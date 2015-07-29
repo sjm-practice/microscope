4796b4b0897d6b4da1366733d1d51766ed68cb67
stevenjmarsh <sjm.atl@gmail.com>
2015-07-29 17:36:41 -0400

    adding more test posts, via fixture

--- server/fixtures.js
+++ server/fixtures.js
@@ -68,6 +68,17 @@ if (Posts.find().count() === 0) {
         commentsCount: 0
     });
 
+    for (var i = 0; i < 15; i++) {
+        Posts.insert({
+            title: 'Test post#' + i,
+            author: sacha.profile.name,
+            userId: sacha._id,
+            url: 'http:/google.com/?q=test-' + i,
+            submitted: new Date(now - i * 3600 * 1000),
+            commentsCount: 0
+        });
+    }
+
     Posts.insert({
         title: 'Bikes are fun.',
         userId: joe._id,
