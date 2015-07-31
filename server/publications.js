a4c5dfa48f5fffb5349376b59d066f7a41781654
stevenjmarsh <sjm.atl@gmail.com>
2015-07-31 11:12:37 -0400

    add limit to posts publication

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,6 @@
-Meteor.publish('posts', function () {
-    return Posts.find();
+Meteor.publish('posts', function (limit) {
+    check(limit, Number);
+    return Posts.find({}, {sort: {submitted: -1}, limit: limit});
 });
 
 Meteor.publish('comments', function (postId) {
