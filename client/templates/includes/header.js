b6c72e90b10583d85f5a076999b97a8c670eef3e
stevenjmarsh <sjm.atl@gmail.com>
2015-08-11 12:00:14 -0400

    refactor: rename header helper

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,5 +1,10 @@
 Template.header.helpers({
-    activeRouteClass: function (/* route names */) {
+    activeClassForRoute: function (/* route names */) {
+        // here are good explanations of how arguments is used
+        // with slice/call here...
+        // arguments - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+        // call - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
+        //        http://stackoverflow.com/questions/7056925/how-does-array-prototype-slice-call-work
         var args = Array.prototype.slice.call(arguments, 0);
         args.pop();
 
